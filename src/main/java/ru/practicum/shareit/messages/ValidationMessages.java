c469a157d8bba7850f7360900e6bd5307bf2516e
LeonovichMariia <MaryKrylova17@yandex.ru>
2023-04-23 21:47:02 +0300

    feat: добавлены мапперы для User и для Item

--- src/main/java/ru/practicum/shareit/messages/ValidationMessages.java
+++ src/main/java/ru/practicum/shareit/messages/ValidationMessages.java
@@ -5,4 +5,6 @@ public interface ValidationMessages {
     String EMPTY_DESCRIPTION = "Описание не может быть пустым";
     String END_DATA = "Дата конца бронирования должна дыть в настоящем или будущем";
     String REQUEST_CREATED_DATA = "Дата создания запроса должна быть в прошлом или настоящем";
+    String INCORRECT_EMAIL = "Некорректный email";
+    String EMPTY_EMAIL = "email не может быть пустым";
 }
